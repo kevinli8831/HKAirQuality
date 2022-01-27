4d7825b60e2a24f0dadc3f4e529aa16607d04500
kevinli8831 <kevinli8831@Gmail.com>
2022-01-27 17:08:25 +0800

    google map

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -13,37 +13,73 @@
     </div>
     <br>
     <GmapMap
+        class="test"
         :center='center'
-        :zoom='11.8'
-        style='width:100%;  height: 600px;'
+        :options="MapOption"
+        @click="temp"
+        style='width:100%;  height: 600px;position: relative'
     >
       <GmapMarker
           :key="index"
           v-for="(m, index) in markers"
           :position="m.position"
-          @click="center=m.position"
+          :clickable="true"
+          @click="clickMarker(m)"
       />
+      <MyLocationButton @click.native="geolocate()"  style="position: absolute;bottom: 110px;right: 10px"/>
+
     </GmapMap>
   </div>
 </template>
 
 <script>
+import {pick} from "lodash"
+import demoLocation from "../demo/demoLocation.json"
+import MyLocationButton from "@/components/GoogleMapLocationButton/MyLocationButton";
+
 export default {
   name: 'GoogleMap',
+  components: {MyLocationButton},
   data() {
     return {
-      center: { lat: 45.508, lng: -73.587 },
+      center: {lat: 22.317722417309373, lng: 114.17434897929937},
       currentPlace: null,
       markers: [],
       places: [],
     }
   },
   mounted() {
+    this.fetchLocation();
     this.geolocate();
   },
+
+  computed: {
+
+    MapOption() {
+      return {
+        streetViewControl: false,
+        zoom: 11.8,
+      }
+    }
+  },
   methods: {
+    pick,
+    temp(event){
+      console.log(event.latLng.lat())
+      console.log(event.latLng.lng())    },
+    fetchLocation() {
+      // demo, getting api
+      demoLocation.locations.forEach(x => {
+        let res = pick(x, ['lat', 'lng'])
+        this.markers.push({position: res});
+      })
+    },
     setPlace(place) {
+      console.log(place, 'place')
       this.currentPlace = place;
+    },
+    clickMarker(m) {
+      this.center = m.position;
     },
     addMarker() {
       if (this.currentPlace) {
@@ -51,20 +87,30 @@ export default {
           lat: this.currentPlace.geometry.location.lat(),
           lng: this.currentPlace.geometry.location.lng(),
         };
-        this.markers.push({ position: marker });
+        this.markers.push({position: marker});
         this.places.push(this.currentPlace);
         this.center = marker;
         this.currentPlace = null;
       }
     },
-    geolocate: function() {
+    geolocate: function () {
       navigator.geolocation.getCurrentPosition(position => {
         this.center = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
+        console.log(this.currentPlace,'place')
+        console.log(this.finalLatLng,'place')
+        console.log(this.center,'this.center')
       });
     },
   },
 };
 </script>
+<style lang="scss" scoped>
+.test{
+  ::v-deep .vue-map-hidden{
+  display: block;
+  }
+}
+</style>
---
+++ src/components/GoogleMapLocationButton/MyLocationButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="LocationButton d-flex justify-center">
+      <v-icon>
+        mdi-crosshairs-gps
+      </v-icon>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyLocationButton"
+}
+</script>
+
+<style scoped>
+.LocationButton {
+  width: 40px;
+  height: 40px;
+  border-radius: 2px;
+  background-color: #fff;
+  box-shadow: 0 1px 8px #9CC8EA ;
+  overflow: hidden;
+  transition: background-color .16s ease-out;
+  cursor: pointer
+}
+</style>
